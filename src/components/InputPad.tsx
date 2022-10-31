335a3797f3752ef3b62b1441513bd5cef78545a7
DaoZhang1588 <1205308740@qq.com>
2022-10-31 23:03:27 +0800

    剩下的明天做

--- src/components/InputPad.tsx
+++ src/components/InputPad.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import {useState} from "react";
 
-const Wrapper=styled.div`
+const Wrapper = styled.div`
   flex-grow: 1;
-  .buttons{
+
+  .buttons {
     font-size: 25px;
     display: grid;
     grid-template: 
@@ -10,52 +12,66 @@ const Wrapper=styled.div`
   "n4 n5 n6 d"
   "n7 n8 n9 s"
   "n0 n0 p s";
-          height: 100%;
-    background:white;
-    border-top:1px solid #ddd;
+    height: 100%;
+    background: white;
+    border-top: 1px solid #ddd;
     grid-auto-rows: 48px;
     grid-auto-columns: 1fr;
     gap: 1px;
-    >button{
+
+    > button {
       border: none;
       background: white;
-      &:nth-child(1){
+
+      &:nth-child(1) {
         grid-area: n1;
       }
-      &:nth-child(2){
+
+      &:nth-child(2) {
         grid-area: n2;
       }
-      &:nth-child(3){
+
+      &:nth-child(3) {
         grid-area: n3;
       }
-      &:nth-child(4){
+
+      &:nth-child(4) {
         grid-area: n4;
       }
-      &:nth-child(5){
+
+      &:nth-child(5) {
         grid-area: n5;
       }
-      &:nth-child(6){
+
+      &:nth-child(6) {
         grid-area: n6;
       }
-      &:nth-child(7){
+
+      &:nth-child(7) {
         grid-area: n7;
       }
-      &:nth-child(8){
+
+      &:nth-child(8) {
         grid-area: n8;
       }
-      &:nth-child(9){
+
+      &:nth-child(9) {
         grid-area: n9;
       }
-      &:nth-child(10){
+
+      &:nth-child(10) {
         grid-area: n0;
       }
-      &:nth-child(11){
+
+      &:nth-child(11) {
         grid-area: p;
       }
-      &:nth-child(12){
+
+      &:nth-child(12) {
         grid-area: d;
       }
-      &:nth-child(13){
+
+      &:nth-child(13) {
         grid-area: s;
         background: #5C33BE;
         color: white;
@@ -64,23 +80,32 @@ const Wrapper=styled.div`
   }
 `
 
-const InputPad=()=>{
-    return(
-        <Wrapper >
+const InputPad = () => {
+    const [amountNumber, setAmountNumber] = useState('')
+    const inputNumberList = [
+        {text: '1', onClick: () => setAmountNumber(amountNumber + '1')},
+        {text: '2', onClick: () => setAmountNumber(amountNumber + '2')},
+        {text: '3', onClick: () => setAmountNumber(amountNumber + '3')},
+        {text: '4', onClick: () => setAmountNumber(amountNumber + '4')},
+        {text: '5', onClick: () => setAmountNumber(amountNumber + '5')},
+        {text: '6', onClick: () => setAmountNumber(amountNumber + '6')},
+        {text: '7', onClick: () => setAmountNumber(amountNumber + '7')},
+        {text: '8', onClick: () => setAmountNumber(amountNumber + '8')},
+        {text: '9', onClick: () => setAmountNumber(amountNumber + '9')},
+        {text: '0', onClick: () => setAmountNumber(amountNumber + '0')},
+        {text: '.', onClick: () => setAmountNumber(amountNumber + '.')},
+        {text: '清空', onClick: () => setAmountNumber('')},
+        {text: '提交', onClick: () => setAmountNumber(amountNumber + 'ok')},
+    ]
+    return (
+        <Wrapper>
             <div className='buttons'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>.</button>
-            <button>清空</button>
-            <button>提交</button>
+                {inputNumberList.map(button =>
+                    <button key={button.text}
+                            onClick={button.onClick}>
+                        {button.text}
+                    </button>
+                )}
             </div>
         </Wrapper>
     )
--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,6 +1,5 @@
 import Calculator from "./Calculator";
 import styled from "styled-components";
-
 const CalculatorWrapper=styled.div`
   flex-grow: 1;
 `
