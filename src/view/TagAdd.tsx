77c6c42743a53fb8fa9813484aeeec198c6cadcc
DaoZhang1588 <1205308740@qq.com>
2022-11-05 19:51:51 +0800

    完成添加标签页面title的CSS

--- src/view/TagAdd.tsx
+++ src/view/TagAdd.tsx
@@ -1,6 +1,61 @@
+import styled from "styled-components";
+import Icon from "../components/Icon";
+
+const Wrapper=styled.div`
+height: 100vh;
+`
+const Header=styled.header`
+    display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid red;
+  padding: 10px 10px;
+  .titleText{
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    .icon{
+      margin-right: 10px;
+    }
+  }
+  button{
+    background: inherit;
+    border: none;
+    margin-right: 10px;
+  }
+`
+const Nav=styled.nav`
+`
+const Main=styled.main`
+`
+const ListSection=styled.section`
+`
 const TagAdd=()=>{
     return(
-        <div>你好</div>
+        <Wrapper>
+            <Header>
+                <span className='titleText'>
+                    <Icon name='back' />
+                    <span>添加支出类别</span>
+                </span>
+                <button>完成</button>
+            </Header>
+            <Nav>
+                <span>选中标签：{<Icon name='money' />}</span>
+                <span>零食</span>
+            </Nav>
+            <Main>
+                <ListSection>
+                <div>餐饮</div>
+                <ul>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                </ul>
+                </ListSection>
+            </Main>
+        </Wrapper>
     )
 }
 
