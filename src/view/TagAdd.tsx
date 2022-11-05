06f861bfa22b07c4d063bd66b374aae1f984555c
DaoZhang1588 <1205308740@qq.com>
2022-11-05 20:06:52 +0800

    完成tagAdd基本样式

--- src/view/TagAdd.tsx
+++ src/view/TagAdd.tsx
@@ -3,12 +3,13 @@ import Icon from "../components/Icon";
 
 const Wrapper=styled.div`
 height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
 const Header=styled.header`
     display: flex;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid red;
   padding: 10px 10px;
   .titleText{
     display: flex;
@@ -25,10 +26,42 @@ const Header=styled.header`
   }
 `
 const Nav=styled.nav`
+    display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 10px;
+  border-bottom: 1px solid gray;
+  .navIcon{
+    display: flex;
+    align-items: center;
+  }
 `
 const Main=styled.main`
+  flex-grow: 1;
+  overflow: auto;
 `
 const ListSection=styled.section`
+    div{
+      text-align: center;
+      padding: 10px 0;
+      font-size: 16px;
+    }
+  
+  ul{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 6px 0;
+    li{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      width: 20%;
+      flex-shrink: 0;
+      margin: 5px 0;
+      
+    }
+  }
 `
 const TagAdd=()=>{
     return(
@@ -41,13 +74,44 @@ const TagAdd=()=>{
                 <button>完成</button>
             </Header>
             <Nav>
-                <span>选中标签：{<Icon name='money' />}</span>
+                <span className='navIcon'>
+                    <span>选中标签：</span>
+                    <Icon name='money' />
+                </span>
                 <span>零食</span>
             </Nav>
             <Main>
                 <ListSection>
                 <div>餐饮</div>
                 <ul>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
+                    <li>
+                        <Icon name='money' />
+                        <span>钱</span>
+                    </li>
                     <li>
                         <Icon name='money' />
                         <span>钱</span>
